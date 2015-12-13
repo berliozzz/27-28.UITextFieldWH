f13516d2a0ef9400f0b5736cd7823e7f6fde614f
Berlioz <kolesichek@mail.ru>
2015-12-13 14:08:44 +0300

    work done

--- TextFieldWH27-28/TextFieldWH27-28/ViewController.m
+++ TextFieldWH27-28/TextFieldWH27-28/ViewController.m
@@ -90,11 +90,91 @@
     
     if ([textField isEqual:self.phoneNumberField])
     {
-        self.phoneNumberLabel.text = resultString;
+        NSCharacterSet *validationSet = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
+        NSArray *components = [string componentsSeparatedByCharactersInSet:validationSet];
+        
+        if ([components count] > 1)
+        {
+            return NO;
+        }
+        
+        NSString *newString = [textField.text stringByReplacingCharactersInRange:range withString:string];
+        
+        NSArray *validComponents = [newString componentsSeparatedByCharactersInSet:validationSet];
+        
+        newString = [validComponents componentsJoinedByString:@""];
+        
+        static const int localNumberMaxLenght = 7;
+        static const int areaCodeMaxLenght = 3;
+        static const int countryCodeMaxLenght = 1;
+        
+        if ([newString length] > localNumberMaxLenght + areaCodeMaxLenght + countryCodeMaxLenght)
+        {
+            return NO;
+        }
+        
+        NSMutableString *phoneNumberString = [NSMutableString string];
+        
+        NSInteger localNumberLenght = MIN([newString length], localNumberMaxLenght);
+        
+        //local number
+        if (localNumberLenght > 0)
+        {
+            NSString *number = [newString substringFromIndex:(int)[newString length] - localNumberLenght];
+            
+            [phoneNumberString appendString:number];
+            
+            if ([phoneNumberString length] > 3)
+            {
+                [phoneNumberString insertString:@"-" atIndex:3];
+            }
+        }
+        //area code number
+        if ([newString length] > localNumberMaxLenght)
+        {
+            NSInteger areaCodeLenght = MIN((int)[newString length] - localNumberMaxLenght, areaCodeMaxLenght);
+            
+            NSRange areaRange = NSMakeRange((int)[newString length] - localNumberMaxLenght - areaCodeLenght, areaCodeLenght);
+            
+            NSString *area = [newString substringWithRange:areaRange];
+            
+            area = [NSString stringWithFormat:@"(%@) ", area];
+            
+            [phoneNumberString insertString:area atIndex:0];
+        }
+        //country code  number
+        if ([newString length] > localNumberMaxLenght + areaCodeMaxLenght)
+        {
+            NSInteger countryCodeLenght = MIN((int)[newString length] - localNumberMaxLenght - areaCodeMaxLenght, countryCodeMaxLenght);
+            
+            NSRange countryCodeRange = NSMakeRange(0, countryCodeLenght);
+            
+            NSString *countryCode = [newString substringWithRange:countryCodeRange];
+            
+            countryCode = [NSString stringWithFormat:@"+%@ ", countryCode];
+            
+            [phoneNumberString insertString:countryCode atIndex:0];
+        }
+        
+        textField.text = phoneNumberString;
+        self.phoneNumberLabel.text = phoneNumberString;
+        
+        return NO;
     }
     
     if ([textField isEqual:self.emailField])
     {
+        NSString *emailString = [textField.text stringByReplacingCharactersInRange:range withString:string];
+        
+        NSCharacterSet *setCheck = [NSCharacterSet characterSetWithCharactersInString:@"@"];
+        
+        NSArray *components = [emailString componentsSeparatedByCharactersInSet:setCheck];
+        
+        if ([components count] > 2)
+        {
+            return NO;
+        }
+        
         self.emailLabel.text = resultString;
     }
     
